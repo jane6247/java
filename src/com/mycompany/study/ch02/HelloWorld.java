11d41be83f001e72bfe9c159a9dda30a8beddfde
jane6247 <jane6247@gmail.com>
2023-05-24 15:40:05 +0900

    first

---
+++ src/com/mycompany/study/ch02/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.mycompany.study.ch02;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HelloWorld"); 
+		System.out.println("이름: 김다래"); 
+		System.out.println("나이: 31세"); 
+		System.out.println("주소: 김포시");
+		
+
+	}
+
+}

ec9d0cb7e4375faae34f46fe3c054a084cd507a0
jane6247 <jane6247@gmail.com>
2023-05-25 16:44:33 +0900

    java

---
+++ src/com/mycompany/study/ch02/ExInt.java
@@ -0,0 +1,17 @@
+package com.mycompany.study.ch02;
+
+public class ExInt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int var1 = 10; //십진수
+		int var2 = 10; //8진수
+		int var3 = 10; //16진수
+		
+		System.out.println("var1: " + var1);
+		System.out.println("var2: " + var1);
+		System.out.println("var3: " + var1);
+	}
+
+}

99b60dcb09fd1ddb3a6ce8a4531ecd28e24fce79
jane6247 <jane6247@gmail.com>
2023-05-26 17:33:48 +0900

    test

---
+++ src/com/mycompany/study/ch02/ExConstants.java
@@ -0,0 +1,22 @@
+package com.mycompany.study.ch02;
+
+public class ExConstants {
+	
+	public static final String MY_NAME = "Tony";
+	public static final Double PAI = 3.14;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// public static final String MY_NAME = "Tony";
+		
+		System.out.println("MY_NAME: " + MY_NAME);
+		
+		// MY_NAME = "Andrew";  //error
+		
+		//상수는 한번 밖에
+		
+
+	}
+
+}

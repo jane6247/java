ea7614bf8fd8eae7178289a69f6705fc7648a576
jane6247 <janekim6247@gmail.com>
2023-06-20 16:17:27 +0900

    java

--- src/com/mycompany/study/ch13/third/Util.java
+++ src/com/mycompany/study/ch13/third/Util.java
@@ -2,7 +2,7 @@ package com.mycompany.study.ch13.third;
 
 public class Util {
 
-	public <T> Box <T> boxing(T t) {
+	public static <T> Box <T> boxing(T t) {
 		Box<T> box = new Box<>();
 		return box;
 
